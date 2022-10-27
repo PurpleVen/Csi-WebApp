9704884dbba50d6ca765b8ca9400113788a8d413
S.Vendra <86104620+PurpleVen@users.noreply.github.com>
2022-10-27 21:56:13 +0530

    test aws

--- CSI-Dashboard/server.js
+++ CSI-Dashboard/server.js
@@ -32,7 +32,9 @@ app.use(cookieParser());
 app.use(express.json({}))
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/images",express.static("images"));
+app.use(express.static(__dirname + '/images'));
+
+<!--app.use("/images",express.static("images"));-->
 app.use("/css",express.static("css"));
 app.use("/scss",express.static("scss"));
 app.use("/pages",express.static("pages"));
